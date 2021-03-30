4e039a84b18d45360ca7e034a65f2d9e66d054cc
jiminp0421 <jm00476@naver.com>
2021-03-30 16:51:16 +0900

    사용자에게 어떻게 출력할것인지 (한글출력)

--- src/com/newlecture/web/Nana.java
+++ src/com/newlecture/web/Nana.java
@@ -16,7 +16,13 @@ public class Nana extends HttpServlet{
 	protected void service(HttpServletRequest req,
 			HttpServletResponse resp) throws ServletException, IOException {
 		
-	 PrintWriter out = resp.getWriter();
-	 out.println("Hello~~~");
+	 
+	resp.setCharacterEncoding("UTF-8");	//UTF-8로 보내는것
+	resp.setContentType("text/html; charset=UTF-8"); //브라우저에게 요대로 해석해라
+	PrintWriter out = resp.getWriter();
+	 
+	 for(int i=0; i<100; i++) {
+		 out.println((i+1)+": 안녕 Servlet!");
+	 }
 	}
 }
